f5af137e6418c307c68f33d9c125c9de015b4982
vamshidhar <80639104+vamshidhar-kasulabada@users.noreply.github.com>
2024-05-18 08:25:39 +0530

    Update

---
+++ create.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+main()
+{
+int fd,n,m;
+char buf1[100],buf2[100];
+fd=open("v.txt",O_WRONLY|O_CREAT,0600); 
+printf("enter a string:");
+fflush(stdin);
+scanf("%s",buf1);
+n=write(fd,buf1,strlen(buf1));
+printf("\n no of bytes written: %d",n);
+close(fd);
+fd=open("v.txt",O_RDONLY); 
+m=read(fd,buf2,n);
+printf("\n no of bytes read:%d",m);
+buf2[n]='\0';
+printf("\n readed string :%s\n",buf2);
+close(fd);
+}
