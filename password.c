ea771d63b79e2cda36d22e3f803e3e00612cc14c
vamshidhar <80639104+vamshidhar-kasulabada@users.noreply.github.com>
2024-05-18 08:22:55 +0530

    Update

---
+++ password.c
@@ -0,0 +1,32 @@
+#include <pwd.h>
+#include <stdio.h>
+#include <sys/types.h>
+main()
+{
+struct passwd *ptr;
+int uid, gid;
+uid = getuid();
+gid = getgid();
+printf("\nUser ID and Group ID using 
+getuid(),getgid()");
+printf("\nUser ID = %d", uid);
+printf("\nGroup ID = %d", gid);
+printf("\nPassword information using 
+getpwuid()");
+ptr = getpwuid(uid);
+printf("\nUsername = %s\nPassword = 
+%s", ptr->pw_name, ptr->pw_passwd);
+printf("\nUser ID = %d\nGroup ID 
+= %d", ptr->pw_uid, ptr->pw_gid);
+printf("\nHome Directory = %s\nShell = 
+%s", ptr->pw_dir, ptr->pw_shell);
+printf("\nPassword information using 
+getpwnam()");
+ptr = getpwnam("mca02-31");
+printf("\nUsername = %s\nPassword = 
+%s", ptr->pw_name, ptr->pw_passwd);
+printf("\nUser ID = %d\nGroup ID 
+= %d", ptr->pw_uid, ptr->pw_gid);
+printf("\nHome Directory = %s\nShell = 
+%s", ptr->pw_dir, ptr->pw_shell);
+}
